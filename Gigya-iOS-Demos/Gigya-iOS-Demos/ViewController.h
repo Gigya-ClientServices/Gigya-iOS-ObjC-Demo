14a4c905d6488aa6fee44c50238b624e47fc447e
Jay Reardon <jayreardon@gmail.com>
2014-12-22 11:19:54 -0800

    Initial Commit

---
+++ Gigya-iOS-Demos/Gigya-iOS-Demos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gigya-iOS-Demos
+//
+//  Created by Jay Reardon on 12/22/14.
+//  Copyright (c) 2014 Gigya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Gigya-iOS-Demos/Gigya-iOS-Demos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Gigya-iOS-Demos
+//
+//  Created by Jay Reardon on 12/22/14.
+//  Copyright (c) 2014 Gigya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
