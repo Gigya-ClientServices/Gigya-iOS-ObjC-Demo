fbf0c8bd0b86ad375b31602920a57ad6b9ecf176
Giovanni Alvarez <giovanni.alvarez@gigya-inc.com>
2014-12-24 11:02:08 -0800

    Added publishUserAction functionality

--- Gigya-iOS-Demos/Gigya-iOS-Demos/PublishActionController.m
+++ Gigya-iOS-Demos/Gigya-iOS-Demos/PublishActionController.m
@@ -7,6 +7,7 @@
 //
 
 #import "PublishActionController.h"
+#import <GigyaSDK/Gigya.h>
 
 @interface PublishActionController ()
 
@@ -27,6 +28,40 @@
 }
 
 - (IBAction)publishButtonAction:(id)sender {
+    
+    if (![Gigya session]) {
+        UIAlertView *alert;
+        alert = [[UIAlertView alloc] initWithTitle:@"Alert"
+                                           message:@"You must be logged in to do that!"
+                                          delegate:nil
+                                 cancelButtonTitle:@"OK"
+                                 otherButtonTitles:nil];
+        [alert show];
+    } else {
+        NSMutableDictionary *userAction = [NSMutableDictionary dictionary];
+        [userAction setObject:@"Gigya iOS SDK Demos" forKey:@"title"];
+        [userAction setObject:self.shareText.text forKey:@"description"];
+        
+        NSError *error;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:userAction
+                                                           options:0
+                                                             error:&error];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        
+        
+        GSRequest *request = [GSRequest requestForMethod:@"socialize.publishUserAction"];
+        [request.parameters setObject:jsonString forKey:@"userAction"];
+        [request sendWithResponseHandler:^(GSResponse *response, NSError *error) {
+            if (!error) {
+                NSLog(@"Success");
+                // Success! Use the response object.
+            }
+            else {
+                NSLog(@"error");
+                // Check the error code according to the GSErrorCode enum, and handle it.
+            }
+        }];
+    }
 }
 
 @end
